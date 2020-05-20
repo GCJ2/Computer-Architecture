d10fd2343477f4961f079c0fddc64c54cca8fa24
Greg Johnston <gcjohnston91@gmail.com>
2020-05-20 13:13:16 -0400

    MVP done with notes

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,10 +7,37 @@ import sys
 
 
 # We need LDI, PRN, and HLT
+# PUSH and POP are needed
+# R7 is reserved as the stack pointer (SP)
 LDI = 0b10000010  # Set Value
 PRN = 0b01000111  # Print Value
 HLT = 0b00000001  # Stop CPU
 MUL = 0b10100010  # Multiply two registers together
+PUSH = 0b01000101  # Push
+POP = 0b01000110  # Pop
+SP = 7
+
+"""
+PUSH:
+Push the value in the given register on the stack:
+
+Decrement the SP.
+Copy the value in the given register to the address pointed to by SP.
+
+POP:
+Pop the value at the top of the stack into the given register:
+
+Copy the value from the address pointed to by SP to the given register.
+Increment SP
+
+
+Per the spec, stack starts at R7/F3
+Stack pointer points to what is at the top of the stack
+Stack grows down, so pointer decrements when something is added to it
+Empty stack points to F3
+Stack with one item points to F3
+Stack with two items points to F2
+"""
 
 # Registry, Memory/RAM, Program Counter needed
 
@@ -59,9 +86,10 @@ class CPU:
 				command = command[0].strip()  # Removes whitespaces
 				# print(command)
 				# command = int(commands) This makes everything base 10, instead of base 2
-				if command == '':   # This just i
+				if command == '':  # This just i
 					continue
 				command = int(command, 2)
+				# print(command)
 				self.ram[address] = command
 				# print(self.ram[address])
 				address += 1
@@ -110,11 +138,13 @@ class CPU:
 	def run(self):
 		running = True
 		while running:
+			self.trace()
 			command = self.ram[self.pc]  # Current command
 			a = self.ram_read(self.pc + 1)  # Passes into ram_read the register index to be read after the command
 			b = self.ram_read(self.pc + 2)  # Passes into ram_read the value to be found
 			if command == LDI:
 				self.reg[a] = b
+				# print(f'Added {b} to {self.reg[a]}')
 				self.pc += 3
 			elif command == PRN:
 				print(self.reg[a])
@@ -122,6 +152,19 @@ class CPU:
 			elif command == MUL:
 				self.alu('MUL', a, b)
 				self.pc += 3
+			elif command == PUSH:
+				selected_reg = self.ram_read(self.pc + 1)  # Find current register
+				value = self.reg[selected_reg]  # Get the value out of it
+				self.reg[SP] -= 1  # Decrement the stack pointer
+				self.ram_write(self.reg[SP], value)  # At the SP location in RAM, add the value from the reg
+				self.pc += 2  # Increment by two, as is two-bit OP
+			elif command == POP:
+				selected_reg = self.ram_read(self.pc + 1)  # Find selected register
+				value = self.ram_read(self.reg[SP])  # Get the value of whatever is stored at the SP
+				self.reg[selected_reg] = value  # Set the selected_reg in the register to the value taken from the SP
+				self.reg[SP] += 1  # Increment the SP
+				self.pc += 2  # Increment PC by two, as POP is a two-bit operation
+
 			elif command == HLT:
 				running = False
 			else:
